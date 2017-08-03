a31e5187a0046a7ceca748d51006da5f602c2914
Alan Casey <alancasey20@gmail.com>
2017-08-02 22:35:29 -0400

    added database to YahooAuctionHelperConfiguration class

--- src/main/java/ie/alancasey/YahooAuctionHelperConfiguration.java
+++ src/main/java/ie/alancasey/YahooAuctionHelperConfiguration.java
@@ -2,9 +2,25 @@ package ie.alancasey;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class YahooAuctionHelperConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @Valid
+    @NotNull
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory factory){
+        this.dataSourceFactory = factory;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory(){
+        return this.dataSourceFactory;
+    }
 }
